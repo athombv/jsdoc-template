18a06408693f9b60dbbeb9901df863fc895eefec
Harwin Borger <harwin.borger@athom.nl>
2021-05-12 17:19:55 +0200

    Add functionality to disable Sticky TOC if the TOC is to long

--- static/scripts/toc.js
+++ static/scripts/toc.js
@@ -2,6 +2,10 @@
   "use strict"
 
   documentReady(function () {
+    const $toc = document.getElementsByClassName('toc');
+    shouldStickyBeDisabled($toc[0]);
+
+    /* Table of content scroll to links */
     const tocLinks = document.getElementsByClassName('toc__link');
     let hashCheck = false;
 
@@ -27,4 +31,33 @@
       });
     });
   })
+
+  /**
+   * @param target
+   * @description Disable position sticky when the TOC is to long to fit in the current viewport
+   */
+  function shouldStickyBeDisabled(target){
+    if(!target){
+      return;
+    }
+
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+
+    let callback = (entries, observer) => {
+      entries.forEach(entry => {
+        if(!entry.isIntersecting){
+          target.classList.add('disable-sticky');
+        }else{
+          target.classList.remove('disable-sticky');
+        }
+      });
+    };
+    let observer = new IntersectionObserver(callback, options);
+
+    observer.observe(target);
+  }
 })();
