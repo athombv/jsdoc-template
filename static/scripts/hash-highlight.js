76b25a81a8cbee7855f03df92165a9ff54bd9783
Harwin Borger <harwin.borger@athom.nl>
2022-05-02 09:28:37 +0200

    cleanup code

--- static/scripts/hash-highlight.js
+++ static/scripts/hash-highlight.js
@@ -9,8 +9,6 @@
   });
 
   function setHighlight() {
-    // removeHighlight();
-
     const id = window.location.hash.split('#')[1];
 
     const component = document.getElementById(id);
@@ -19,16 +17,9 @@
     }
 
     setTimeout(function () {
-      // todo set check on UID to avoid canceling to early.
-      // removeHighlight(component);
       component.classList.remove('is-highlight');
     }, 2000);
   }
 
-  // function removeHighlight() {
-  //   const highlights = document.getElementsByClassName('is-highlight');
-  //   for (var i = 0; i < highlights.length; i++) {
-  //   }
-  // }
 
 }());
