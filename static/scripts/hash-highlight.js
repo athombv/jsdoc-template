de5bc08360b33919132294ac65484ffb6e61ffc2
Harwin Borger <harwin.borger@athom.nl>
2022-05-02 09:22:20 +0200

    finish hash highlight

--- static/scripts/hash-highlight.js
+++ static/scripts/hash-highlight.js
@@ -3,13 +3,12 @@
   documentReady(function () {
     setHighlight();
     window.addEventListener('highlight', () => {
-      console.log('hashchange');
       setHighlight()
     });
   });
 
   function setHighlight() {
-    removeHighlight();
+    // removeHighlight();
 
     const id = window.location.hash.split('#')[1];
 
@@ -20,16 +19,16 @@
     }
 
     setTimeout(function () {
-      // todo set check on UID to avoid canceling to early. 
-      removeHighlight();
+      // todo set check on UID to avoid canceling to early.
+      // removeHighlight(component);
+      component.classList.remove('is-highlight');
     }, 2000);
   }
 
-  function removeHighlight() {
-    const highlights = document.getElementsByClassName('is-highlight');
-    for (var i = 0; i < highlights.length; i++) {
-      highlights[i].classList.remove('is-highlight');
-    }
-  }
+  // function removeHighlight() {
+  //   const highlights = document.getElementsByClassName('is-highlight');
+  //   for (var i = 0; i < highlights.length; i++) {
+  //   }
+  // }
 
 }());
